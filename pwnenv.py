fcf8de2e3981373592a12785c2dc1c707591c7bd
D4m0n <d4m0n.8@gmail.com>
2020-11-25 02:59:21 +0900

    parsing sub-command "run"

--- pwnenv.py
+++ pwnenv.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 import os
 import sys
 import argparse
@@ -35,15 +36,13 @@ def build(args):
             make_dockerfile(args)
             if not build_image(args):
                 print('[!] Failed to build images.')
-                sys.exit(0)    
+                sys.exit(0)
+    else:
+        print('[!] The image already exists')
 
 
 def run_container(args):
-    pass
-
-
-def stop_container(args):
-    pass
+    print('test')
 
 
 def get_shell(args):
@@ -51,54 +50,57 @@ def get_shell(args):
 
 
 def clean(args):
+    print('clean')
     pass
 
 
 def main():
     parser = argparse.ArgumentParser(description='Pwnable environment based Docker.')
-    sub_parser = parser.add_subparsers(help='sub-command')
+    pwnenv_help = lambda args: parser.print_help()
+    parser.set_defaults(func=pwnenv_help)
+    sub_parser = parser.add_subparsers(dest='subparser_name', help='sub-command')
 
     parser_build = sub_parser.add_parser('build', help='build pwnenv image')
     parser_run = sub_parser.add_parser('run', help='run conatiner')
     parser_stop = sub_parser.add_parser('stop', help='stop container')
     parser_clean = sub_parser.add_parser('clean', help='clean images or containers')
 
-    parser_build.add_argument('version', type=float, choices=[16.04, 18.04, 20.04], default=20.04, help='environment image version(default: 20.04)')
-    parser_build.set_defaults(func=build_image)
+    parser_build.add_argument('version', type=float, choices=[16.04, 18.04, 20.04], nargs='?', default=20.04, help='environment image version(default: 20.04)')
+    parser_build.set_defaults(func=build)
 
-    #parser_run.add_argument('version', type=float, choices=[16.04, 18.04, 20.04], default=20.04, help='environment image version(default: 20.04)')
-    parser_run.add_argument('-u', '--user', default='ubuntu', help='users to run the target binary(default: ubuntu)')
-    parser_run.add_argument('-r', '--remote', action='store_true', help='for remote environment')
-    parser_run.add_argument('-d', '--debugging', action='store_true', help='for local debugging environment')
-    parser_run.add_argument('-p', '--port', type=int, help='port fowarding for remote')
+    parser_run.add_argument('version', type=float, choices=[16.04, 18.04, 20.04], nargs='?', default=20.04, help='environment image version(default: 20.04)')
+    parser_run.add_argument('-b', '--binary', help='target binary with remote or local')
+    run_remote = parser_run.add_argument_group('remote')
+    remote_group = run_remote.add_mutually_exclusive_group()
+    remote_group.add_argument('-r', '--remote', action='store_true', help='for remote environment')
+    remote_group.add_argument('-d', '--debugging', action='store_true', help='for debugging core file from crashed remote')
+    run_remote.add_argument('-p', '--port', type=int, default=1234, help='port fowarding for remote(default: 1234)')
+    run_remote.add_argument('-u', '--user', default='ubuntu', help='users to run the target binary(default: ubuntu)')
+    run_local = parser_run.add_argument_group('local')
+    local_group = run_local.add_mutually_exclusive_group()
+    local_group.add_argument('-l', '--local', action='store_true', help='for local environment(run a binary)')
+    local_group.add_argument('-s', '--shell', action='store_true', help='for local environment(spawn a shell)')
+    local_group.add_argument('-v', '--volume', help='mount a volume')
+    parser_run.set_defaults(func=run_container)
 
     parser_clean.add_argument('-i', '--images', action='store_true', help='clean images')
     parser_clean.add_argument('-c', '--containers', action='store_true', help='clean container')
+    parser_clean.set_defaults(func=clean)
+
     args = parser.parse_args()
-    print()
+
+    if args.subparser_name == 'remote':
+        if not args.remote or args.debugging or args.local or args.shell:
+            parser_run.error('one of the arguments -r/--remote -d/--debugging -l/--local -s/--shell is required')
+
+        if (args.remote or args.debugging) and (args.local or args.shell):
+            parser_run.error('not allowed when used remote and local both')
+
+        if (args.remote or args.local) and not args.binary:
+            parser_run.error('the following arguments are required: -b/--binary')
+
+    args.func(args)
     
-    '''
-    if 'build' in args.command and 'run' in args.command:
-        if not exists_image(args):
-            make_dockerfile(args)
-            if not build_image(args):
-                print('[!] Failed to build images.')
-                sys.exit(0)
-        if 'run' in args.command:
-            run_container(args)
-    elif 'stop' in args.command:
-        stop_container(args)
-    elif 'shell' in args.command:
-        get_shell(args)
-    elif 'clean' in args.command:
-        if args.images or args.containers:
-            clean(args)
-        else:
-            '[!] Choose what you want to clean'
-            sys.exit(0)
-    else:
-        pass
-    '''
 
 if __name__ == '__main__':
     main()
